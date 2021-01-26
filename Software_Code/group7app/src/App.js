c46ff50a5ea668f7c8b53ae9ec93b770efbb5774
alivesurfin <alivesurfin@gmail.com>
2021-01-26 10:32:44 +0000

    This commit creates the basic template for the front end
    -added react routing
    -added Home , Questionnaire , Login pages with empty css

--- Software_Code/group7app/src/App.js
+++ Software_Code/group7app/src/App.js
@@ -1,24 +1,51 @@
-import logo from './logo.svg';
-import './App.css';
-
+import React from "react";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+import Home from "./Home/Home";
+import Login from "./Login/Login";
+import Questionnaire from "./Questionnaire/Questionnaire";
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div>
+        <ul>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/login">Login</Link>
+          </li>
+          <li>
+            <Link to="/questionnaire">Questionnaire</Link>
+          </li>
+        </ul>
+
+        <hr />
+
+        {/*
+          A <Switch> looks through all its children <Route>
+          elements and renders the first one whose path
+          matches the current URL. Use a <Switch> any time
+          you have multiple routes, but you want only one
+          of them to render at a time
+        */}
+        <Switch>
+          <Route exact path="/">
+            <Home></Home>
+          </Route>
+          <Route path="/login">
+            <Login></Login>
+          </Route>
+          <Route path="/questionnaire">
+           <Questionnaire></Questionnaire>
+          </Route>
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
---
+++ Software_Code/group7app/src/Questionnaire/Questionnaire.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+import './Questionnaire.css'
+export default class Questionnaire extends Component {
+    render() {
+        return (
+            <div>
+                Questionnaire Page
+            </div>
+        )
+    }
+}

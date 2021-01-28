6545a59f80c546cc8003a4cd66446b878fa9c595
TSalten <saltenis.tadas@gmail.com>
2021-01-28 19:55:40 +0200

    [fix] Added escaping in responses
    Github issue #1

--- Software_Code/group7backend/db/index.js
+++ Software_Code/group7backend/db/index.js
@@ -65,7 +65,7 @@ database.createQuiz = (req) => {
 			let questionID = results[0][Object.keys(results[0])[0]];
 
 			for (let i in question.value.responses) {
-				let response = question.value.responses[i];
+				let response = question.value.responses[i].replace("\\", "\\\\");
 				pool.query(`INSERT INTO Responses (questionnairesID, questionID, responseValue, orderID) ` +
 				`VALUES (${questionnaireID}, ${questionID}, '${response.value}', ${response.id});`, (err, res) => {
 					if (err) {

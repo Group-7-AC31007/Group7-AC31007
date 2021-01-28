915fa0801f25ce6014004c35696721ebb8db10a0
alivesurfin <alivesurfin@gmail.com>
2021-01-28 14:48:22 +0000

    [feat] User able to login, only able to access auth'd pages if so - login state stored in cookies - persists accross refresh
    Github issue #14

--- Software_Code/group7app/src/App.js
+++ Software_Code/group7app/src/App.js
@@ -1,4 +1,5 @@
-import React from "react";
+import React, { useState, withRouter } from "react";
+import Cookies from 'js-cookie'
 import {
   BrowserRouter as Router,
   Switch,
@@ -10,15 +11,21 @@ import Login from "./Login/Login";
 import Questionnaire from "./Questionnaire/Questionnaire";
 
 let callApi = async (endpoint = "") => {
-    const response = await fetch('http://localhost:3001/' + endpoint);
-	const body = await response.json();
-    if (response.status !== 200) throw Error(body.message);
-    
-    return body;
-};
+  const response = await fetch('http://localhost:3001/' + endpoint);
+  const body = await response.json();
+  if (response.status !== 200) throw Error(body.message);
 
+  return body;
+};
 function App() {
-	callApi("test").then(res => console.log(res)).catch(err => console.log(err));
+  callApi("test").then(res => console.log(res)).catch(err => console.log(err));
+  console.log("reloading-app");
+  let userStateStart = ""
+  if(!!Cookies.get('access_token')){
+    userStateStart = Cookies.get('access_token').split("#")[0]
+  }
+  const [user, setUser] = useState(userStateStart)
+  
   return (
     <Router>
       <div>
@@ -43,17 +50,17 @@ function App() {
           you have multiple routes, but you want only one
           of them to render at a time
         */}
-        <Switch>
+        
           <Route exact path="/">
             <Home></Home>
           </Route>
           <Route path="/login">
-            <Login></Login>
+            <Login user={user} userCallback={(a) => setUser(a)}></Login>
           </Route>
           <Route path="/questionnaire">
-           <Questionnaire></Questionnaire>
+            <Questionnaire user={user}></Questionnaire>
           </Route>
-        </Switch>
+        
       </div>
     </Router>
   );
--- Software_Code/group7app/src/Questionnaire/Questionnaire.js
+++ Software_Code/group7app/src/Questionnaire/Questionnaire.js
@@ -1,11 +1,27 @@
 import React, { Component } from 'react'
 import './Questionnaire.css'
+import Cookies from 'js-cookie'
+import { Redirect } from 'react-router-dom'
 export default class Questionnaire extends Component {
+    constructor(props) {
+        super(props)
+        this.state = { user: props.user }
+    }
     render() {
-        return (
-            <div>
-                Questionnaire Page
-            </div>
-        )
+        console.log(Cookies.get('access_token') );
+        console.log(this.state.user+"#logged-in" == Cookies.get('access_token'));
+        if (Cookies.get('access_token') == this.state.user + "#logged-in") {
+            return (
+                <div>
+                    Questionnaire Page
+                    {this.state.user}
+                </div>
+            )
+        } else {
+            return (
+               <Redirect to ="/login"></Redirect>
+            )
+        }
+
     }
 }

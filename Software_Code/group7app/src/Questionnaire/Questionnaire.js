a4b00f90417bb207a9c234d17f78c18c59829b74
alivesurfin <alivesurfin@gmail.com>
2021-01-29 12:39:11 +0000

    [feat][fix] storting id, cleanup on login exit -user id is now stored to be used when grabbing from database -cleanup on exit of login page because settimout was leaking
    Github issue #14

--- Software_Code/group7app/src/Questionnaire/Questionnaire.js
+++ Software_Code/group7app/src/Questionnaire/Questionnaire.js
@@ -16,12 +16,12 @@ export default class Questionnaire extends Component {
     constructor(props) {
         super(props)
         this.history = props.history
-        this.state = { user: props.user }
+        this.state = { user: props.user.user,id:props.user.id }
     }
     render() {
         console.log(Cookies.get('access_token') );
-        console.log(this.state.user+"#logged-in" == Cookies.get('access_token'));
-        if (Cookies.get('access_token') == this.state.user + "#logged-in") {
+        console.log();
+        if (this.state.user+"#"+this.state.id+"#logged-in" == Cookies.get('access_token')) {
             return (
                 <div>
                    <Router>

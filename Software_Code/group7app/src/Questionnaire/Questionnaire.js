0cd8fededec4ee86e0915b28b23d780046020186
alivesurfin <alivesurfin@gmail.com>
2021-02-01 09:38:08 +0000

    [fix] fixed a merge error - missing a }

--- Software_Code/group7app/src/Questionnaire/Questionnaire.js
+++ Software_Code/group7app/src/Questionnaire/Questionnaire.js
@@ -16,52 +16,53 @@ export default class Questionnaire extends Component {
     constructor(props) {
         super(props)
         this.history = props.history
-        this.state = { user: props.user.user,id:props.user.id }
+        this.state = { user: props.user.user, id: props.user.id }
     }
     render() {
-        console.log(Cookies.get('access_token') );
-        if (this.state.user+"#"+this.state.id+"#logged-in" == Cookies.get('access_token')) {
+        console.log(Cookies.get('access_token'));
+        if (this.state.user + "#" + this.state.id + "#logged-in" == Cookies.get('access_token')) {
 
 
             return (
                 <div className="quest-wrapper">
-                   <Router>
-                <div className="quest-parent-wrapper">
-                    <div className="questionnaireCreator-label">
-                        <Link className="questionnaireCreator-label-text quest-nav-item" to="/questionnaireCreator"> Questionnaire Creator </Link>
-                        <Link className= "questionnaireTaker-label-text quest-nav-item" to="/questionnaireTaker"> Questionnaire Taker </Link>
-                    </div>
-                        
+                    <Router>
+                        <div className="quest-parent-wrapper">
+                            <div className="questionnaireCreator-label">
+                                <Link className="questionnaireCreator-label-text quest-nav-item" to="/questionnaireCreator"> Questionnaire Creator </Link>
+                                <Link className="questionnaireTaker-label-text quest-nav-item" to="/questionnaireTaker"> Questionnaire Taker </Link>
+                            </div>
 
-                    {/*
+
+                            {/*
                     A <Switch> looks through all its children <Route>
                     elements and renders the first one whose path
                     matches the current URL. Use a <Switch> any time
                     you have multiple routes, but you want only one
                     of them to render at a time
                     */}
-                    <Switch>
-                        <Route path="/questionnaireCreator">
-                            <QuestionnaireCreator>  </QuestionnaireCreator>
-                        </Route>
-                        <Route path="/questionnaireTaker">
-                            <QuestionnaireTaker>  </QuestionnaireTaker>
-                        </Route>
-                    </Switch>
-                </div>
-            </Router>
+                            <Switch>
+                                <Route path="/questionnaireCreator">
+                                    <QuestionnaireCreator>  </QuestionnaireCreator>
+                                </Route>
+                                <Route path="/questionnaireTaker">
+                                    <QuestionnaireTaker>  </QuestionnaireTaker>
+                                </Route>
+                            </Switch>
+                        </div>
+                    </Router>
                 </div>
             )
         } else {
-            
-                this.history.push("/login")
-            
+
+            this.history.push("/login")
+
             return (
-              <div>
-                  Redirecting to login
-              </div>
-              )
-        
+                <div>
+                    Redirecting to login
+                </div>
+            )
+
+        }
     }
 }
 

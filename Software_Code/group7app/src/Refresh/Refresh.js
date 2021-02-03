dfc225142cbe0077e4558d89e5c95b2fa397aedf
ellie99 <elisaveta.peeva@gmail.com>
2021-02-03 13:24:17 +0000

    [design] Changes in redirecting design
    - changes in redirecting page design

--- Software_Code/group7app/src/Refresh/Refresh.js
+++ Software_Code/group7app/src/Refresh/Refresh.js
@@ -32,12 +32,11 @@ export default class Refresh extends Component {
         return (
             <div className="redirectWrapper">
                 <div className="redirectContainer">
-                    {/*
                     <h1>
                         {this.state.message == "" ? "Redirecting ..." : this.state.message}
-                    </h1> */}
+                    </h1> 
 
-                    <Loader color="#4365e2" width={200} height={200} type="Oval" />
+                    <Loader color="#4365e2" width={150} height={150} type="Oval" />
                 </div>
             </div>
         )

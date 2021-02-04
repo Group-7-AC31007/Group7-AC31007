1585a5e52f5a1c53c9003bc562b7386a4d95af6a
ellie99 <elisaveta.peeva@gmail.com>
2021-02-04 15:51:45 +0000

    Start to the Home Page
    - start to home page and HomePage branch

--- Software_Code/group7app/src/Home/Home.js
+++ Software_Code/group7app/src/Home/Home.js
@@ -9,7 +9,7 @@ export default class Home extends Component {
     }
     render() {
         return (
-            <div>
+            <div className="home-page-wrapper">
                 
             </div>
         )

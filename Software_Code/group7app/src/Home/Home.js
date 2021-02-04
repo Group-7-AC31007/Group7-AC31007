b9583324cb5e5d7b8d36dba6e27805a6c283bd7b
ellie99 <elisaveta.peeva@gmail.com>
2021-02-04 16:20:31 +0000

    Start in development of Home Page

--- Software_Code/group7app/src/Home/Home.js
+++ Software_Code/group7app/src/Home/Home.js
@@ -1,5 +1,13 @@
-import React, { Component } from 'react'
+import React, { useState, withRouter } from "react";
+import Cookies from 'js-cookie'
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
 import './Home.css'
+import Login from "../Login/Login";
 export default class Home extends Component {
     constructor(props) {
         super(props);
@@ -10,7 +18,11 @@ export default class Home extends Component {
     render() {
         return (
             <div className="home-page-wrapper">
-                
+                <div className="home-page-square-one"> 
+                    <Link to="/login" onClick={(e) => { dropDown(e) }} >{!(Cookies.get('access_token') == user.user + "#" + user.id + "#" + user.position + "#logged-in") ? "Login" : "Sign Out"} </Link>
+                </div>
+                <Route path="/login" component={loginWrapper} />
+                <div className="home-page-square-two"> </div>
             </div>
         )
     }
